d63fac4d01139f3598040ac790e6827f988b55d0
Megan <megan.vishal@hotmail.com>
2020-07-28 13:19:19 -0700

    Initial Commit

---
+++ Politify-FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Politify-FinalProject
+//
+//  Created by Apple on 7/28/20.
+//  Copyright © 2020 Megan Vishal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
