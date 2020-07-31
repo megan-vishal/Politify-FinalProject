80da7c09a3970499366ea3a5d1a9f33adf13fbe6
Deepa <deepamahesh4391@gmail.com>
2020-07-31 02:12:14 -0700

    Finished aesthetic decorations, changed the coloring for the text to just make it transparent. I also deleted the back buttons because the user can just swipe down to go to recent pages. I kept the next button for the state bill though so they can see the cosponsors. I left the Governors section as it is.  I hope the text transparent thing is ok

---
+++ Politify-FinalProject/stateBillsFirePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  stateBillsFirePageViewController.swift
+//  Politify-FinalProject
+//
+//  Created by Deepa Mahesh on 7/30/20.
+//  Copyright © 2020 Megan Vishal. All rights reserved.
+//
+
+import UIKit
+
+class stateBillsFirePageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
